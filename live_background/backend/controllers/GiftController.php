cab4644512cefd2c837d2a04879a8ed96bf48b63
15030997019 <1255427859@qq.com>
2017-07-19 10:36:28 +0800

    昨晚的更新

--- live_background/backend/controllers/GiftController.php
+++ live_background/backend/controllers/GiftController.php
@@ -8,49 +8,83 @@ use yii\filters\AccessControl;
 
 class GiftController extends Controller
 {
+    public $enableCsrfValidation = false;
+    /*
+        导航添加
+     */
+    public function actionAdd()
+    {
+        if($data = Yii::$app->request->post()){
+            if(isset($_POST['gift_money']) and isset($_POST['gift_name'])){
+                $db=yii::$app->db;
+                $sql="select gift_id from gift where gift_name='$_POST[gift_name]'";
+                $sql=$db->createCommand($sql)->queryOne();
+                if(!$sql){
+                    $sqls="insert into gift (gift_name,gift_money) values ('$_POST[gift_name]','$_POST[gift_money]')";
+                    $sqls=$db->createCommand($sqls)->execute();
+                    if($sqls){
+                        $this->redirect('?r=gift/list');
+                    }
+                }
+            }
+            return $this->render('add');
+        }else{
+            return $this->render('add');
+        }
+    }
 
-	/*
-		导航添加
-	 */
-	public function actionAdd()
-	{
-		if($data = Yii::$app->request->post()){
 
-		}else{
-			return $this->render('add');
-		}
-	}
+    /*
+        导航列表
+     */
+    public function actionList()
+    {
+        if($data = Yii::$app->request->post()){
 
-	/*
-		导航列表
-	 */
-	public function actionList()
-	{
-		if($data = Yii::$app->request->post()){
+        }else{
+            $db=yii::$app->db;
+            $sql="select * from gift";
+            $sql=$db->createCommand($sql)->queryAll();
+            return $this->render('list',array('gift'=>$sql));
+        }
+    }
 
-		}else{
-			return $this->render('list');
-		}
-	}
+    /*
+        导航修改
+     */
+    public function actionSave()
+    {
+        if($data = Yii::$app->request->post()){
+//        print_r($_POST);
+            if(isset($_POST['gift_id'])){
+                $db=yii::$app->db;
+                if(empty($_POST['gift_name']) or empty($_POST['gift_money'])){
 
-	/*
-		导航修改
-	 */
-	public function actionSave()
-	{
-		if($data = Yii::$app->request->post()){
+                }else{
+                    $sql="update gift set gift_name='$_POST[gift_name]',gift_money='$_POST[gift_money]' where gift_id='$_POST[gift_id]'";
+                    $db->createCommand($sql)->execute();
+                }
+            }
 
-		}else{
-			return $this->render('save');
-		}
-	}
+            $this->redirect("?r=gift/save&gift=$_POST[gift_id]");
+        }else{
 
-	/*
-		导航删除
-	 */
-	public function actionDel()
-	{
+            $db=yii::$app->db;
+            $sql="select * from gift where gift_id='$_GET[gift]'";
+            $sql=$db->createCommand($sql)->queryOne();
+            return $this->render('save',array('gift'=>$sql));
+        }
+    }
 
-	}
+    /*
+        导航删除
+     */
+    public function actionDel()
+    {
+        $db=yii::$app->db;
+        $sql="delete from gift where gift_id='$_GET[gift]'";
+        $sql=$db->createCommand($sql)->execute();
+        $this->redirect('?r=gift/list');
+    }
 
 }
--- live_background/backend/views/gift/add.php
+++ live_background/backend/views/gift/add.php
@@ -1,77 +1,86 @@
 <div class="main-content">
-					<div class="breadcrumbs" id="breadcrumbs">
+    <div class="breadcrumbs" id="breadcrumbs">
 
-						<ul class="breadcrumb">
-							<li>
-								<i class="icon-home home-icon"></i>
-								<a href="#">Home</a>
-							</li>
-							<li class="active">Nav</li>
-						</ul><!-- .breadcrumb -->
+        <ul class="breadcrumb">
+            <li>
+                <i class="icon-home home-icon"></i>
+                <a href="#">Home</a>
+            </li>
+            <li class="active">Nav</li>
+        </ul><!-- .breadcrumb -->
 
-						<div class="nav-search" id="nav-search">
-							<form class="form-search">
+        <div class="nav-search" id="nav-search">
+            <form class="form-search">
 								<span class="input-icon">
 									<input placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" type="text">
 									<i class="icon-search nav-search-icon"></i>
 								</span>
-							</form>
-						</div><!-- #nav-search -->
-					</div>
+            </form>
+        </div><!-- #nav-search -->
+    </div>
 
-					<div class="page-content">
-						<div class="page-header">
-							<h1>
-								Nav
-								<small>
-									<i class="icon-double-angle-right"></i>
-									add
-								</small>
-							</h1>
-						</div><!-- /.page-header -->
+    <div class="page-content">
+        <div class="page-header">
+            <h1>
+                Nav
+                <small>
+                    <i class="icon-double-angle-right"></i>
+                    add
+                </small>
+            </h1>
+        </div><!-- /.page-header -->
 
-						<div class="row">
-							<div class="col-xs-12">
-								<!-- PAGE CONTENT BEGINS -->
+        <div class="row">
+            <div class="col-xs-12">
+                <!-- PAGE CONTENT BEGINS -->
 
-								<form class="form-horizontal" role="form">
+                <form class="form-horizontal" role="form" method="post" action="?r=gift/add">
 
-									<div class="form-group">
-										<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> Nav Name </label>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label no-padding-right" for="form-field-1"> 礼物名称 </label>
 
-										<div class="col-sm-9">
-											<div class="col-xs-12 col-sm-3">
-												<input id="form-field-1" v-model="name" placeholder=" nav name " class="col-xs-10 col-sm-12" type="text">
-											</div>
-										</div>
-									</div>
+                        <div class="col-sm-9">
+                            <div class="col-xs-12 col-sm-3">
+                                <input id="form-field-1" v-model="name" name="gift_name" placeholder=" nav name.. " class="col-xs-10 col-sm-12" type="text">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-3 control-label no-padding-right" for="form-field-1"> 礼物价格 </label>
 
-									<div class="space-4"></div>
+                        <div class="col-sm-9">
+                            <div class="col-xs-12 col-sm-3">
+                                <input id="form-field-1" v-model="name" name="gift_money" placeholder=" price.. " class="col-xs-10 col-sm-12" type="text">
+                            </div>
+                        </div>
+                    </div>
 
-									<div class="clearfix form-actions">
-										<div class="col-md-offset-2 col-md-9">
-											<button class="btn btn-info" type="button">
-												<i class="icon-ok bigger-110"></i>
-												创建
-											</button>
+                    <div class="space-4"></div>
 
-											&nbsp; &nbsp; &nbsp;
-											<button class="btn" type="reset">
-												<i class="icon-undo bigger-110"></i>
-												重置
-											</button>
-										</div>
-									</div>
+                    <div class="clearfix form-actions">
+                        <div class="col-md-offset-2 col-md-9">
+                            <button class="btn btn-info" type="submit">
+                                <i class="icon-ok bigger-110"></i>
+                                创建
+                            </button>
 
-									
+                            &nbsp; &nbsp; &nbsp;
+                            <button class="btn" type="reset">
+                                <i class="icon-undo bigger-110"></i>
+                                重置
+                            </button>
+                        </div>
+                    </div>
 
-									
-								</form>
 
-								
 
-								<!-- PAGE CONTENT ENDS -->
-							</div><!-- /.col -->
-						</div><!-- /.row -->
-					</div>
-				</div>
+
+                </form>
+
+
+
+                <!-- PAGE CONTENT ENDS -->
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div>
+</div>
